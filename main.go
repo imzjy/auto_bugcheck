f96993b59d4c8e00aee0c0c3f617a3402e4a2334
Jerry Chou <imjatsz@gmail.com>
2019-07-19 17:29:46 +0800

    add cdb to parameter -p, make sure the cdb and log folder exist before trying to get the bug check

--- main.go
+++ main.go
@@ -6,20 +6,36 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
 	"strings"
 )
 
+const cdbPath string = `C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\cdb.exe`
+
 func main() {
 
 	var logFolder string
+	var cdb string
 	flag.StringVar(&logFolder, "d", "", "log folder contains DMP files")
+	flag.StringVar(&cdb, "p", cdbPath, "cdb file path")
 	flag.Parse()
 
-	if logFolder == "" {
-		fmt.Println("Usage:")
+	if logFolder == "" || cdb == "" {
+		fmt.Fprintf(os.Stderr, "usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		return
+	}
+
+	if fileNotExist(cdb) {
+		fmt.Fprintf(os.Stderr, "cdb not found.\n\tat location: %s", cdb)
+		return
+	}
+
+	if fileNotExist(logFolder) {
+		fmt.Fprintf(os.Stderr, "log folder not found.\n\tat location: %s", logFolder)
 		return
 	}
 
@@ -31,14 +47,14 @@ func main() {
 	for _, file := range files {
 		if strings.ToLower(filepath.Ext(file.Name())) == ".dmp" {
 			dmpFile := path.Join(logFolder, file.Name())
-			bugCheck := getBugCheckStr(dmpFile)
+			bugCheck := getBugCheckStr(cdb, dmpFile)
 			fmt.Printf("%s\n\t%s\n\n", dmpFile, bugCheck)
 		}
 	}
 }
 
-func getBugCheckStr(dmpFile string) string {
-	output, err := exec.Command("cdb", "-z", dmpFile, `-c`, `!analyze -v;q`).Output()
+func getBugCheckStr(cdb string, dmpFile string) string {
+	output, err := exec.Command(cdb, "-z", dmpFile, `-c`, `!analyze -v;q`).Output()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -52,4 +68,9 @@ func getBugCheckStr(dmpFile string) string {
 	}
 
 	return ""
+}
+
+func fileNotExist(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return os.IsNotExist(err)
 }
